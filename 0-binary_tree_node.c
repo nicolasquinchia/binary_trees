5d84c47ec7384a8fde54390ef0f7e3081467e7cb
nicolasquinchia <nicolasquinchia@gmail.com>
2020-07-21 22:32:31 -0500

    Binary Trees, New tree node, check values

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,16 +11,31 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_tree_node;
 
-	if (!value)
-	{
-		return (NULL);
-	}
 	new_tree_node = malloc(sizeof(binary_tree_t));
 	if (new_tree_node == NULL)
 	{
 		return (NULL);
 	}
-	new_tree_node->parent = parent;
-	new_tree_node->n = value;
+	if (parent == NULL) /* It is because is ROOT NODE */
+	{
+		new_tree_node->parent = parent;
+		new_tree_node->n = value;
+		return (new_tree_node);
+	}
+	else /* Have to check the values of the nodes */
+	{
+		if (value > parent->n && parent->right == NULL)
+		{
+			new_tree_node->parent = parent;
+			new_tree_node->n = value;
+			parent->right = new_tree_node;
+		}
+		else if (value < parent->n && parent->left == NULL)
+		{
+			new_tree_node->parent = parent;
+			new_tree_node->n = value;
+			parent->left = new_tree_node;
+		}
+	}
 	return (new_tree_node);
 }
