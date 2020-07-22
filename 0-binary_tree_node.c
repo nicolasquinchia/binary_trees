15c47989c6d966e740433c9b7e7ba0d6f3b21939
nicolasquinchia <nicolasquinchia@gmail.com>
2020-07-21 19:44:41 -0500

    Binary Trees, New tree node, check if the vaule dosen't exist

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,6 +11,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_tree_node;
 
+	if (!value)
+	{
+		return NULL;
+	}
 	new_tree_node = malloc(sizeof(binary_tree_t));
 	if (new_tree_node == NULL)
 	{
