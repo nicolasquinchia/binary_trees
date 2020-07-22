b1bac060d3d4b6c36ae910b866b6349008b6a45c
nicolasquinchia <nicolasquinchia@gmail.com>
2020-07-21 19:45:41 -0500

    Binary Trees, New tree node, check betty style

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	if (!value)
 	{
-		return NULL;
+		return (NULL);
 	}
 	new_tree_node = malloc(sizeof(binary_tree_t));
 	if (new_tree_node == NULL)
