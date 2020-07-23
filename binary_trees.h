8d075932b2a0088ed88f9b78209c591e1d867390
nicolasquinchia <nicolasquinchia@gmail.com>
2020-07-23 01:42:49 -0500

    Binary Trees, tree delete

--- binary_trees.h
+++ binary_trees.h
@@ -36,11 +36,13 @@ typedef struct binary_tree_s heap_t;
 /* FUNCTIONS */
 /* Binary tree printer */
 void binary_tree_print(const binary_tree_t *);
-
 /* New node */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 /* Insert left */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 /* Insert right */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+/* Tree delete */
+void binary_tree_delete(binary_tree_t *tree);
+
 #endif /* BINARY_TREES_H */
