ae20b4e01c85afc12497291121914025820dd6cc
nicolasquinchia <nicolasquinchia@gmail.com>
2020-07-23 20:05:41 -0500

    Binary Trees, check if is a leaf

--- binary_trees.h
+++ binary_trees.h
@@ -44,5 +44,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 /* Tree delete */
 void binary_tree_delete(binary_tree_t *tree);
+/* Is a Leaf */
+int binary_tree_is_leaf(const binary_tree_t *node);
 
 #endif /* BINARY_TREES_H */
